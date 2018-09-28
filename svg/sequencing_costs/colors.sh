676976e47a0cd43fe507178959c596edf7823caf
Lucas Czech <luc@s-cze.ch>
2018-09-28 16:22:52 +0200

    Add Intro Movtivation section

---
+++ svg/sequencing_costs/colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i 's/rgb(255,0,102)/rgb(88,162,255)/g' sequencing_costs.svg
+sed -i 's/rgb(102,102,255)/rgb(255,87,137)/g' sequencing_costs.svg
+
+# blue
+# rgb(102,102,255)
+# rgb(88,162,255)
+
+# red
+# rgb(255,0,102)
+# rgb(255,87,137)
